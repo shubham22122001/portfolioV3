adcb7910abf53f1c298be8a3458219b5261fd111
Shubkash <shubkash@example.com>
2024-06-11 10:52:27 +0530

    "second commit"

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'bhadra.sh@northeastern.edu',
-    phone: '+919619925149',
+    phone: '+91961992',
     address: 'Boston,MA,USA ',
     github: 'https://github.com/shubham22122001',
     linkedIn: 'https://www.linkedin.com/in/shubham-bhadra-4a1b121b1/',
